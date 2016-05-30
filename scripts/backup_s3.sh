2d72d76fc1f7f3c29bc01e63bc624b976ffe4107
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-05-30 10:10:47 +0200

    Fix backup script

--- scripts/backup_s3.sh
+++ scripts/backup_s3.sh
@@ -2,9 +2,9 @@
 
 set -e -u
 
-mkdir -p "hex-s3"
-rm -rf logs-fastly || true
-rm -rf logs-s3 || true
+rm -rf logs* || true
+mkdir -p hex-s3
+mkdir -p logs
 
 today=$(date "+%Y-%m-%d")
 yesterday=$(date "+%Y-%m-%d" --date=yesterday)
@@ -14,19 +14,17 @@ aws s3 sync s3://s3.hex.pm hex-s3 --delete --include "*" --quiet
 
 echo ""
 echo "### logs s3 ###"
-mkdir -p logs-s3
-aws s3 cp s3://logs.hex.pm      logs-s3                                --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
-aws s3 cp s3://logs-eu.hex.pm   logs-s3 --source-region eu-west-1      --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
-aws s3 cp s3://logs-asia.hex.pm logs-s3 --source-region ap-southeast-1 --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
-echo logs-s3/* | xargs cat > logs-s3.txt
+aws s3 cp s3://logs.hex.pm      logs                         --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
+aws s3 cp s3://logs-eu.hex.pm   logs --region eu-west-1      --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
+aws s3 cp s3://logs-asia.hex.pm logs --region ap-southeast-1 --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
+echo logs/hex/* | xargs cat > logs-s3.txt
 gzip -9 logs-s3.txt
 
 echo ""
 echo "### logs fastly ###"
-mkdir -p logs-fastly
-aws s3 cp s3://logs.hex.pm logs-fastly --recursive --exclude "*" --include "fastly_hex/${yesterday}-*" --quiet
-echo logs-fastly/*.gz | xargs gunzip
-echo logs-fastly/* | xargs cat > logs-fastly.txt
+aws s3 cp s3://logs.hex.pm logs --recursive --exclude "*" --include "fastly_hex/${yesterday}T*" --quiet
+echo logs/fastly_hex/*.gz | xargs gunzip
+echo logs/fastly_hex/* | xargs cat > logs-fastly.txt
 gzip -9 logs-fastly.txt
 
 echo ""
@@ -37,4 +35,4 @@ aws s3 cp logs-fastly.txt.gz s3://backup.hex.pm/logs/daily/hex-fastly-${yesterda
 
 echo ""
 echo "### clean up ###"
-rm -rf logs-s3* logs-fastly*
+rm -rf logs*
