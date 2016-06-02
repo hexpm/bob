881e531a0fd7d5c4e67f811e8624b6d78ffaf37a
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-06-02 12:41:31 +0200

    Clone first in docs script

--- scripts/elixir_docs.sh
+++ scripts/elixir_docs.sh
@@ -2,14 +2,14 @@
 
 set -e -u
 
+git clone git://github.com/elixir-lang/elixir.git --depth 1 --single-branch
+git clone git://github.com/elixir-lang/ex_doc.git --depth 1 --single-branch
+git clone https://${BOB_GITHUB_TOKEN}@github.com/elixir-lang/docs.git
+
 PATH=$(pwd)/elixir/bin:${PATH}
 MIX_ARCHIVES=$(pwd)/.mix
 
 mix local.hex --force
-
-git clone git://github.com/elixir-lang/elixir.git --depth 1 --single-branch
-git clone git://github.com/elixir-lang/ex_doc.git --depth 1 --single-branch
-git clone https://${BOB_GITHUB_TOKEN}@github.com/elixir-lang/docs.git
 
 pushd ex_doc
 mix do deps.get, compile
