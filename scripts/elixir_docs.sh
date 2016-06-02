3a3c58c9c1fcb5eb44e3f93caae74b370393baec
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-06-02 02:18:50 +0200

    Fix docs build

--- scripts/elixir_docs.sh
+++ scripts/elixir_docs.sh
@@ -7,13 +7,13 @@ MIX_ARCHIVES=$(pwd)/.mix
 
 mix local.hex --force
 
+git clone git://github.com/elixir-lang/elixir.git --depth 1 --single-branch
 git clone git://github.com/elixir-lang/ex_doc.git --depth 1 --single-branch
+git clone https://${BOB_GITHUB_TOKEN}@github.com/elixir-lang/docs.git
 
 pushd ex_doc
 mix do deps.get, compile
 popd
-
-git clone https://${BOB_GITHUB_TOKEN}@github.com/elixir-lang/docs.git
 
 pushd elixir
 make publish_docs
