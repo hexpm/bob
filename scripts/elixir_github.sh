3baaeef467a91fac6f2bbe4fdf6456e6e5c9517d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-05-16 13:31:51 +0200

    Fix hexdocs fastly purging

--- scripts/elixir_github.sh
+++ scripts/elixir_github.sh
@@ -6,6 +6,8 @@
 set -e -u
 
 APPS=(eex elixir ex_unit iex logger mix)
+HEXPM=$BOB_FASTLY_SERVICE_HEXPM
+HEXDOCS=$BOB_FASTLY_SERVICE_HEXDOCS
 
 function fastly_purge {
   curl \
@@ -13,16 +15,16 @@ function fastly_purge {
     -H "Fastly-Key: ${BOB_FASTLY_KEY}" \
     -H "Accept: application/json" \
     -H "Content-Length: 0" \
-    "https://api.fastly.com/service/${BOB_FASTLY_SERVICE}/purge/${1}"
+    "https://api.fastly.com/service/${1}/purge/${2}"
 }
 
 # $1 = ref
 function build {
-  git clone git://github.com/elixir-lang/elixir.git --quiet --branch ${1}
+  git clone git://github.com/elixir-lang/elixir.git --quiet --branch "${1}"
 
   pushd elixir
 
-  otp $1
+  otp "$1"
   erl +V
   make compile
 
@@ -33,17 +35,17 @@ function upload_build {
   pushd elixir
 
   make Precompiled.zip || make release_zip
-  aws s3 cp *.zip s3://s3.hex.pm/builds/elixir/${1}.zip --acl public-read --cache-control "public,max-age=3600" --metadata '{"surrogate-key":"builds","surrogate-control":"public,max-age=604800"}'
-  fastly_purge builds
+  aws s3 cp *.zip "s3://s3.hex.pm/builds/elixir/${1}.zip" --acl public-read --cache-control "public,max-age=3600" --metadata '{"surrogate-key":"builds","surrogate-control":"public,max-age=604800"}'
+  fastly_purge $HEXPM builds
 
   popd
 }
 
 function upload_docs {
-  version=$(echo ${1} | sed 's/^v//g')
+  version=$(echo "${1}" | sed 's/^v//g')
 
-  MIX_ARCHIVES=${cwd}/.mix
-  PATH=${cwd}/elixir/bin:${PATH}
+  MIX_ARCHIVES="${cwd}/.mix"
+  PATH="${cwd}/elixir/bin:${PATH}"
   elixir -v
 
   mix local.hex --force
@@ -56,7 +58,7 @@ function upload_docs {
   tags=$(git tag)
   latest_version=$(elixir ${scripts}/latest_version.exs "${tags}")
   ex_doc_version=$(elixir ${scripts}/elixir_to_ex_doc.exs "${1}" "${latest_version}")
-  git checkout ${ex_doc_version}
+  git checkout "${ex_doc_version}"
   mix do deps.get, compile --no-elixir-version-check
   popd
 
@@ -70,8 +72,12 @@ function upload_docs {
 
   pushd doc
   for app in "${APPS[@]}"; do
-    aws s3 cp ${app} s3://hexdocs.pm/${app}/${version} --recursive --acl public-read --cache-control "public,max-age=3600" --metadata "{\"surrogate-key\":\"docspage/${app}/${version}\",\"surrogate-control\":\"public,max-age=604800\"}"
-    fastly_purge "docspage/${app}/${version}"
+    aws s3 cp "${app}" "s3://hexdocs.pm/${app}/${version}" --recursive --acl public-read --cache-control "public,max-age=3600" --metadata "{\"surrogate-key\":\"docspage/${app}/${version}\",\"surrogate-control\":\"public,max-age=604800\"}"
+    fastly_purge $HEXDOCS "docspage/${app}/${version}"
+
+    tar -czf "${app}-${version}.tar.gz" -C "${app}" .
+    aws s3 cp "${app}-${version}.tar.gz" "s3://s3.hex.pm/docs/${app}-${version}.tar.gz" --acl public-read --cache-control "public,max-age=3600" --metadata "{\"surrogate-key\":\"docs/${app}-${version}\",\"surrogate-control\":\"public,max-age=604800\"}"
+    fastly_purge $HEXPM "docs/${app}-${version}"
   done
   popd
 
@@ -81,8 +87,8 @@ function upload_docs {
 
     pushd doc
     for app in "${APPS[@]}"; do
-      aws s3 cp ${app} s3://hexdocs.pm/${app} --recursive --acl public-read --cache-control "public,max-age=3600" --metadata "{\"surrogate-key\":\"docspage/${app}\",\"surrogate-control\":\"public,max-age=604800\"}"
-      fastly_purge "docspage/${app}"
+      aws s3 cp "${app}" "s3://hexdocs.pm/${app}" --recursive --acl public-read --cache-control "public,max-age=3600" --metadata "{\"surrogate-key\":\"docspage/${app}\",\"surrogate-control\":\"public,max-age=604800\"}"
+      fastly_purge $HEXDOCS "docspage/${app}"
     done
     popd
   fi
@@ -92,13 +98,16 @@ function upload_docs {
 
 # $1 = ref
 function delete {
-  aws s3 rm s3://s3.hex.pm/builds/elixir/${1}.zip
-  fastly_purge builds
+  aws s3 rm "s3://s3.hex.pm/builds/elixir/${1}.zip"
 
   for app in "${APPS[@]}"; do
-    version=$(echo ${1} | sed 's/^v//g')
-    aws s3 rm s3://hexdocs.pm/${app}/${version} --recursive
-    fastly_purge "docspage/${app}/${version}"
+    version=$(echo "${1}" | sed 's/^v//g')
+    
+    aws s3 rm "s3://s3.hex.pm/docs/${app}-${version}.tar.gz"
+    fastly_purge $HEXPM builds
+
+    aws s3 rm "s3://hexdocs.pm/${app}/${version}" --recursive
+    fastly_purge $HEXDOCS "docspage/${app}/${version}"
   done
 }
 
@@ -106,9 +115,9 @@ function delete {
 function otp {
   rm .tool-versions || true
 
-  otp_version=$(elixir ${scripts}/elixir_to_otp.exs "$1")
+  otp_version=$(elixir "${scripts}/elixir_to_otp.exs" "$1")
   echo "Using OTP ${otp_version}"
-  PATH=${HOME}/.asdf/installs/erlang/${otp_version}/bin:${PATH}
+  PATH="${HOME}/.asdf/installs/erlang/${otp_version}/bin:${PATH}"
 }
 
 cwd=$(pwd)
@@ -116,11 +125,11 @@ scripts="${cwd}/../../scripts"
 
 case "$1" in
   "push" | "create")
-    build $2
-    upload_build $2
-    upload_docs $2
+    build "$2"
+    upload_build "$2"
+    upload_docs "$2"
     ;;
   "delete")
-    delete $2
+    delete "$2"
     ;;
 esac
