1f97560aaceb2046bd4492ca4d9226a37817d7e3
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-10-10 12:08:54 +0200

    Fix logs bucket and print stderr

--- scripts/backup_s3.sh
+++ scripts/backup_s3.sh
@@ -10,19 +10,18 @@ today=$(date "+%Y-%m-%d")
 yesterday=$(date "+%Y-%m-%d" --date=yesterday)
 
 echo "### s3 ###"
-aws s3 sync s3://s3.hex.pm hex-s3 --delete --include "*" --quiet
+aws s3 sync s3://s3.hex.pm hex-s3 --delete --include "*" > /dev/null
 
 echo ""
 echo "### logs s3 ###"
-aws s3 cp s3://logs.hex.pm      logs                         --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
-aws s3 cp s3://logs-eu.hex.pm   logs --region eu-west-1      --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
-aws s3 cp s3://logs-asia.hex.pm logs --region ap-southeast-1 --recursive --exclude "*" --include "hex/${yesterday}-*" --quiet
+aws s3 cp s3://logs.hex.pm      logs                         --recursive --exclude "*" --include "hex/${yesterday}-*" > /dev/null
+aws s3 cp s3://eu.logs.hex.pm   logs --region eu-west-1      --recursive --exclude "*" --include "hex/${yesterday}-*" > /dev/null
 echo logs/hex/* | xargs cat > logs-s3.txt
 gzip -9 logs-s3.txt
 
 echo ""
 echo "### logs fastly ###"
-aws s3 cp s3://logs.hex.pm logs --recursive --exclude "*" --include "fastly_hex/${yesterday}T*" --quiet
+aws s3 cp s3://logs.hex.pm logs --recursive --exclude "*" --include "fastly_hex/${yesterday}T*" > /dev/null
 echo logs/fastly_hex/*.gz | xargs gunzip
 echo logs/fastly_hex/* | xargs cat > logs-fastly.txt
 gzip -9 logs-fastly.txt
