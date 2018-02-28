79fc34a8856e4b92938735220d0bf8bbf583a539
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-02-28 15:59:33 +0100

    Push DB backups to tarsnap instead of S3

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -9,5 +9,5 @@ for app in "${APPS[@]}"; do
   echo "BACKING UP ${app}"
   heroku pg:backups:capture -a ${app}
   heroku pg:backups:download -a ${app} -o ${app}-${today}.dump
-  aws s3 cp ${app}-${today}.dump s3://backup.hex.pm/dbs/daily/${app}-${today}.dump
+  tarsnap -c -f dbdump-${app}-${today} ${app}-${today}.dump
 done
