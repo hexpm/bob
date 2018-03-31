242b97d5a55744119ea681a7a522910c48bf69fc
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-03-31 17:27:55 +0200

    Fix backup S3 directory location

--- lib/bob/job/backup_s3.ex
+++ lib/bob/job/backup_s3.ex
@@ -2,7 +2,7 @@ defmodule Bob.Job.BackupS3 do
   require Logger
 
   def run([]) do
-    directory = Bob.Directory.new()
+    directory = "persist/backup-s3"
     Logger.info("Using directory #{directory}")
     Bob.Script.run({:script, "backup_s3.sh"}, [], directory)
   end
