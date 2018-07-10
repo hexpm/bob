da1192892b7aef75f874ea31612e4048c3106353
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-07-11 01:02:30 +0200

    Update elixir_to_ex_doc.exs

--- scripts/elixir_to_ex_doc.exs
+++ scripts/elixir_to_ex_doc.exs
@@ -3,29 +3,18 @@
 [elixir, latest] = System.argv
 
 case elixir do
-  "v1.4"      -> "v#{latest}"
-  "v1.3"      -> "v#{latest}"
-  "v1.2"      -> "v0.14.1"
-  "v1.1"      -> "v0.12.0"
-  "v1.0"      -> "v0.12.0"
-  "v" <> version ->
-    case Version.parse(version) do
-      {:ok, version} ->
-        cond do
-          Version.compare(version, "1.2.3") in [:eq, :gt] ->
-            "v#{latest}"
-          Version.compare(version, "1.2.0") in [:eq, :gt] ->
-            "v0.14.1"
-          Version.compare(version, "1.0.0") in [:eq, :gt] ->
-            "v0.12.0"
-          true ->
-            "v#{latest}"
-        end
-      :error ->
-        "v#{latest}"
-    end
+  "v1.7" <> _ -> "v#{latest}"
+  "v1.6" <> _ -> "v0.18.3"
+  "v1.5" <> _ -> "v0.18.3"
+  "v1.4" <> _ -> "v0.18.3"
+  "v1.3" <> _ -> "v0.18.3"
+  "v1.2.5"    -> "v0.18.3"
+  "v1.2.4"    -> "v0.18.3"
+  "v1.2.3"    -> "v0.18.3"
+  "v1.2" <> _ -> "v0.14.1"
+  "v1.1" <> _ -> "v0.12.0"
+  "v1.0" <> _ -> "v0.12.0"
   # All branches use master
-  _ ->
-    "master"
+  _ -> "master"
 end
 |> IO.puts
