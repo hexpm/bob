ca39dad4b6242cb199983e8f4c5b172490170831
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-07-27 10:17:02 +0200

    Elixir 1.7 released

--- scripts/elixir_to_ex_doc.exs
+++ scripts/elixir_to_ex_doc.exs
@@ -3,9 +3,7 @@
 [elixir, latest] = System.argv
 
 case elixir do
-  # Temporary until ex_doc v0.19.0 is released
-  "v1.7.0-rc" <> _ -> "master"
-  "v1.7" <> _ -> "master"
+  "v1.7" <> _ -> "v0.19.0"
   "v1.6" <> _ -> "v0.18.3"
   "v1.5" <> _ -> "v0.18.3"
   "v1.4" <> _ -> "v0.18.3"
