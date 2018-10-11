b3670002cf2d7f6a6abab607168a08f21609835c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-10-11 20:16:47 +0200

    Filter OTP versions correctly from builds.txt
    Fixes infinite rebuilds on some bash versions.

--- priv/scripts/otp.sh
+++ priv/scripts/otp.sh
@@ -24,6 +24,7 @@ aws s3 cp ${ref_name}.tar.gz s3://s3.hex.pm/builds/otp/${linux}/${ref_name}.tar.
 
 aws s3 cp s3://s3.hex.pm/builds/otp/${linux}/builds.txt builds.txt || true
 touch builds.txt
+sed -i '/^${ref_name} /d' builds.txt
 echo -e "${ref_name} ${ref}\n$(cat builds.txt)" > builds.txt
 sort -u -k1,1 -o builds.txt builds.txt
 aws s3 cp builds.txt s3://s3.hex.pm/builds/otp/${linux}/builds.txt --cache-control "public,max-age=3600" --metadata '{"surrogate-key":"otp-builds","surrogate-control":"public,max-age=604800"}'
