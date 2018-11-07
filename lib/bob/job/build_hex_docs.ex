5cea3331d910dadbae4e4d7030ea5f1a8252dbc0
Wojtek Mach <wojtek@wojtekmach.pl>
2018-11-07 20:09:53 +0100

    Build Hex docs only on tags

--- lib/bob/job/build_hex_docs.ex
+++ lib/bob/job/build_hex_docs.ex
@@ -2,13 +2,20 @@ defmodule Bob.Job.BuildHexDocs do
   require Logger
 
   def run([event, ref]) do
-    directory = Bob.Directory.new()
-    Logger.info("Using directory #{directory}")
-    Bob.Script.run({:script, "hex_docs.sh"}, [event, ref], directory)
+    if run?(ref) do
+      directory = Bob.Directory.new()
+      Logger.info("Using directory #{directory}")
+      Bob.Script.run({:script, "hex_docs.sh"}, [event, ref], directory)
+    else
+      Logger.info("Skipping hexpm/hex/#{ref}")
+    end
   end
 
   def equal?(_, _), do: false
 
   def similar?(args, args), do: true
   def similar?(_, _), do: false
+
+  defp run?("v" <> string), do: match?({:ok, _}, Version.parse(string))
+  defp run?(_), do: false
 end
