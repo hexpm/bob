7027d3799055adb7bc47b864a2861287835f4c66
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-01-04 10:43:28 +0100

    Name elixir zips correctly

--- priv/scripts/elixir.sh
+++ priv/scripts/elixir.sh
@@ -20,16 +20,16 @@ function push {
   otp_version=${otp_versions[0]}
   otp_string=$(otp_string ${otp_version})
   build "$1" "$2" "${otp_version}" "1"
-  upload_build "$1" "$2" "${otp_string}"
+  upload_build "$1" "${otp_string}"
   upload_docs "$1"
 
   for otp_version in "${otp_versions[@]:1}"; do
     otp_string=$(otp_string ${otp_version})
     build "$1" "$2" "${otp_version}" "0"
-    upload_build "$1" "$2" "${otp_string}"
+    upload_build "$1" "${otp_string}"
   done
 
-  upload_build "$1" "$2" ""
+  upload_build "$1" ""
   update_builds_txt "$1" "$2" ${@:3}
   fastly_purge $BOB_FASTLY_SERVICE_HEXPM builds
 
