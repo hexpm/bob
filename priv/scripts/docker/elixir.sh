a997adf31e2d17acb1c9f359a5438a6c4cdfc1ba
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-01-21 16:06:18 +0100

    Update dockerhub env variables

--- priv/scripts/docker/elixir.sh
+++ priv/scripts/docker/elixir.sh
@@ -9,7 +9,7 @@ alpine=$4
 
 tag=${elixir}-erlang-${erlang}-alpine-${alpine}
 
-docker login docker.io --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
+docker login docker.io --username ${BOB_DOCKERHUB_USERNAME} --password ${BOB_DOCKERHUB_PASSWORD}
 
 docker build -t hexpm/elixir:${tag} --build-arg ELIXIR=${elixir} --build-arg ERLANG_MAJOR=${erlang_major} --build-arg ERLANG=${erlang} --build-arg ALPINE=${alpine} -f ${SCRIPT_DIR}/docker/elixir.dockerfile ${SCRIPT_DIR}
 docker push docker.io/hexpm/elixir:${tag}
--- priv/scripts/docker/erlang.sh
+++ priv/scripts/docker/erlang.sh
@@ -7,7 +7,7 @@ alpine=$2
 
 tag=${erlang}-alpine-${alpine}
 
-docker login docker.io --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
+docker login docker.io --username ${BOB_DOCKERHUB_USERNAME} --password ${BOB_DOCKERHUB_PASSWORD}
 
 docker build -t hexpm/erlang:${tag} --build-arg ERLANG=${erlang} --build-arg ALPINE=${alpine} -f ${SCRIPT_DIR}/docker/erlang.dockerfile ${SCRIPT_DIR}
 docker push docker.io/hexpm/erlang:${tag}
