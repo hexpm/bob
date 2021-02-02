94e54fbb1a8284d7238010524553f79ddbd82c48
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-02-02 13:19:47 +0000

    Add cache timeout

--- lib/docker_hub/cache.ex
+++ lib/docker_hub/cache.ex
@@ -9,19 +9,64 @@ defmodule Bob.DockerHub.Cache do
 
   def init([]) do
     :ets.new(__MODULE__, [:ordered_set, :public, :named_table])
-    {:ok, %{}}
+    {:ok, %{waiting: %{}, locks: %{}}}
   end
 
-  def handle_call({:lock, repo}, from, locks) do
-    case Map.fetch(locks, repo) do
-      {:ok, waiting} -> {:noreply, Map.put(locks, repo, [from | waiting])}
-      :error -> {:reply, :aquired, Map.put(locks, repo, [])}
+  def handle_call({:lock, repo}, from, state) do
+    case Map.fetch(state.waiting, repo) do
+      {:ok, waiters} ->
+        state = %{state | waiting: Map.put(state.waiting, repo, [from | waiters])}
+        {:noreply, state}
+
+      :error ->
+        lock_ref = make_ref()
+        timer_ref = Process.send_after(self(), {:timeout, repo, lock_ref}, @timeout)
+
+        state = %{
+          state
+          | waiting: Map.put(state.waiting, repo, []),
+            locks: Map.put(state.locks, repo, {timer_ref, lock_ref})
+        }
+
+        {:reply, :aquired, state}
     end
   end
 
-  def handle_call({:unlock, repo}, _from, locks) do
-    Enum.each(Map.fetch!(locks, repo), &GenServer.reply(&1, :done))
-    {:reply, :ok, Map.delete(locks, repo)}
+  def handle_call({:unlock, repo}, _from, state) do
+    case Map.fetch(state.locks, repo) do
+      {:ok, {timer_ref, _lock_ref}} ->
+        Enum.each(Map.fetch!(state.waiting, repo), &GenServer.reply(&1, :done))
+        Process.cancel_timer(timer_ref)
+
+      :error ->
+        :ok
+    end
+
+    state = %{
+      state
+      | waiting: Map.delete(state.waiting, repo),
+        locks: Map.delete(state.locks, repo)
+    }
+
+    {:noreply, :ok, state}
+  end
+
+  def handle_info({:timeout, repo, lock_ref}, state) do
+    case Map.fetch(state.locks, repo) do
+      {:ok, {_timer_ref, ^lock_ref}} ->
+        Enum.each(Map.fetch!(state.waiting, repo), &GenServer.reply(&1, :done))
+
+      _ ->
+        :ok
+    end
+
+    state = %{
+      state
+      | waiting: Map.delete(state.waiting, repo),
+        locks: Map.delete(state.locks, repo)
+    }
+
+    {:reply, :ok, state}
   end
 
   def add(repo, tag, archs) do
@@ -31,7 +76,7 @@ defmodule Bob.DockerHub.Cache do
   def lookup(repo, fun) do
     case :ets.lookup(__MODULE__, {:status, repo}) do
       [] ->
-        case GenServer.call(__MODULE__, {:lock, repo}, @timeout) do
+        case GenServer.call(__MODULE__, {:lock, repo}, @timeout * 2) do
           :aquired ->
             result = fun.()
 
