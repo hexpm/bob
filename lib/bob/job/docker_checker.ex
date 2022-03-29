c4f9dada9de67d42881daeb442787c6f413412fa
Ho-Yon Mak <hoyon.mak@gmail.com>
2022-03-29 10:17:11 +0100

    Upgrade to Alpine 3.14.5 and Alpine 3.15.3 (#107)

--- lib/bob/job/docker_checker.ex
+++ lib/bob/job/docker_checker.ex
@@ -8,8 +8,8 @@ defmodule Bob.Job.DockerChecker do
 
   @builds %{
     "alpine" => [
-      "3.14.3",
-      "3.15.0"
+      "3.14.5",
+      "3.15.3"
     ],
     "ubuntu" => [
       "impish-20211102",
