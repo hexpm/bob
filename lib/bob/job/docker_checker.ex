5aa44520b8dab98d0ab6b8748af0a5e19f0585f2
Michał Łępicki <michallepicki@users.noreply.github.com>
2022-05-29 10:31:07 +0200

    mix format

--- lib/bob/job/docker_checker.ex
+++ lib/bob/job/docker_checker.ex
@@ -80,7 +80,10 @@ defmodule Bob.Job.DockerChecker do
   defp build_erlang_ref?("debian", "buster-" <> _, "OTP-1" <> _), do: false
   defp build_erlang_ref?("debian", "bullseye-" <> _, "OTP-1" <> _), do: false
   defp build_erlang_ref?("ubuntu", "focal-" <> _, "OTP-1" <> _), do: false
-  defp build_erlang_ref?("ubuntu", "jammy-" <> _, "OTP-" <> version), do: build_openssl_3?(version)
+
+  defp build_erlang_ref?("ubuntu", "jammy-" <> _, "OTP-" <> version),
+    do: build_openssl_3?(version)
+
   defp build_erlang_ref?(_os, _os_version, _ref), do: true
 
   defp build_erlang_ref?("arm64", "ubuntu", "trusty-" <> _, "OTP-17" <> _), do: false
