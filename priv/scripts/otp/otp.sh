3c9bfc74e8e2b80af602e01e4daa1d5dfc26f3c8
Anthony Accomazzo <accomazz@gmail.com>
2023-04-09 14:30:39 -0700

    Add ARM builds

--- priv/scripts/otp/otp.sh
+++ priv/scripts/otp/otp.sh
@@ -5,12 +5,23 @@ set -euox pipefail
 ref_name=$1
 ref=$2
 linux=$3
+arch=$4
 
 source ${SCRIPT_DIR}/utils.sh
 
-echo "Building $1 $2 $3"
+echo "Building $1 $2 $3 $4"
 
-container="otp-build-${linux}-${ref_name}"
+if [[ "$arch" == "arm64" ]]; then
+  container="otp-build-${linux}-${ref_name}-arm"
+  platform="--platform linux/arm64"
+elif [[ "$arch" == "amd64" ]]; then
+  container="otp-build-${linux}-${ref_name}"
+  platform="--platform linux/amd64"
+else
+  echo "Invalid architecture. Please specify either 'arm64' or 'amd64'."
+  exit 1
+fi
+
 image="bob-otp"
 tag=${linux}
 date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -23,13 +34,14 @@ if [ "$(echo ${ref_name} | cut -d '-' -f 2 | cut -d '.' -f 1)" -le "20" ]; then
   pie_ldflags=""
 fi
 
-docker build \
+docker buildx build ${platform} \
   --ulimit nofile=1024:1024 \
   -t ${image}:${tag} \
   --build-arg OTP_REF=${ref_name} \
   --build-arg PIE_CFLAGS=${pie_cflags} \
   --build-arg PIE_LDFLAGS=${pie_ldflags} \
   -f ${SCRIPT_DIR}/otp/otp-${linux}.dockerfile ${SCRIPT_DIR}
+
 docker rm -f ${container} || true
 docker run -t -e OTP_REF=${ref_name} --name=${container} ${image}:${tag}
 
