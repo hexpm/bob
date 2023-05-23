9bea1989947cd18972a715e77e5a83d55fd59d67
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-05-23 16:06:50 +0200

    Skip old Ubuntus

--- lib/bob/job/otp_checker.ex
+++ lib/bob/job/otp_checker.ex
@@ -1,6 +1,6 @@
 defmodule Bob.Job.OTPChecker do
   @repo "erlang/otp"
-  @linuxes ["ubuntu-14.04", "ubuntu-16.04", "ubuntu-18.04", "ubuntu-20.04", "ubuntu-22.04"]
+  @linuxes ["ubuntu-18.04", "ubuntu-20.04", "ubuntu-22.04"]
 
   def run(_type) do
     for linux <- @linuxes,
