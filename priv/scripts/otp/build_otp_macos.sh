c7e0842ec1213871330a90467614265a473d872b
Wojtek Mach <wojtek@wojtekmach.pl>
2024-08-05 03:26:26 +0200

    Use ref as cache key

--- priv/scripts/otp/build_otp_macos.sh
+++ priv/scripts/otp/build_otp_macos.sh
@@ -16,7 +16,7 @@ EOF
   local arch=$3
   : "${OPENSSL_VERSION:=3.1.6}"
   : "${OPENSSL_DIR:=/tmp/builds/openssl-${OPENSSL_VERSION}-macos-${arch}}"
-  : "${OTP_DIR:=/tmp/builds/otp-${ref_name}-openssl-${OPENSSL_VERSION}-macos-${arch}}"
+  : "${OTP_DIR:=/tmp/builds/otp-${ref_name}-${ref}-openssl-${OPENSSL_VERSION}-macos-${arch}}"
   export MAKEFLAGS=-j$(nproc)
   export CFLAGS="-Os -fno-common -mmacosx-version-min=11.0"
 
@@ -76,10 +76,6 @@ build_otp() {
   local rel_dir=${OTP_DIR}
   local src_dir=/tmp/builds/src-otp-${ref_name}
   local test_cmd="erl -noshell -eval 'io:format(\"~s~s~n\", [erlang:system_info(system_version), erlang:system_info(system_architecture)]), ok = crypto:start(), io:format(\"crypto ok~n\"), halt().'"
-
-  if [[ "$ref_name" == *maint-* || "$ref_name" == maint || "$ref_name" == master ]]; then
-    rm -rf ${rel_dir} ${src_dir}
-  fi
 
   if [ -d "${rel_dir}/bin" ]; then
     echo "${rel_dir}/bin already exists, skipping build"
