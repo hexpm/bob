3871ae54a2fc941980896cf4ce50b83095f27e3d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-06-05 15:27:54 +0200

    Add jobs and job_failures Ecto schemas

---
+++ lib/bob/queue/failure.ex
@@ -0,0 +1,10 @@
+defmodule Bob.Queue.Failure do
+  use Ecto.Schema
+
+  schema "job_failures" do
+    field(:module_key, Bob.Queue.Term)
+    field(:args_digest, :binary)
+    field(:count, :integer)
+    field(:last_failed_at, :utc_datetime_usec)
+  end
+end
---
+++ lib/bob/queue/job.ex
@@ -0,0 +1,13 @@
+defmodule Bob.Queue.Job do
+  use Ecto.Schema
+
+  schema "jobs" do
+    field(:module_key, Bob.Queue.Term)
+    field(:args, Bob.Queue.Term)
+    field(:args_digest, :binary)
+    field(:state, :string)
+    field(:inserted_at, :utc_datetime_usec)
+    field(:started_at, :utc_datetime_usec)
+    field(:finished_at, :utc_datetime_usec)
+  end
+end
