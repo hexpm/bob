85d27d661ed8da926617f5588c6963c82ac39470
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-06-05 20:17:00 +0200

    Remove unused Bob.Job equal?/2 and similar?/2 callbacks
    Declared but never implemented or called.

--- lib/bob/job.ex
+++ lib/bob/job.ex
@@ -2,6 +2,4 @@ defmodule Bob.Job do
   @type args :: [term()]
 
   @callback run(args()) :: term()
-  @callback equal?(args(), args()) :: boolean()
-  @callback similar?(args(), args()) :: boolean()
 end
