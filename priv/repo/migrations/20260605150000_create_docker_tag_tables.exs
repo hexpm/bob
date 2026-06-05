d7c45497e65d948e84418952b5b0a68d8f4324e0
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-06-05 17:02:59 +0200

    Add docker_tags and base_image_tags migrations

---
+++ priv/repo/migrations/20260605150000_create_docker_tag_tables.exs
@@ -0,0 +1,21 @@
+defmodule Bob.Repo.Migrations.CreateDockerTagTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:docker_tags) do
+      add :repo, :string, null: false
+      add :tag, :string, null: false
+      add :archs, {:array, :string}, null: false
+      add :built_at, :utc_datetime_usec, null: false
+    end
+
+    create unique_index(:docker_tags, [:repo, :tag])
+
+    create table(:base_image_tags) do
+      add :repo, :string, null: false
+      add :tag, :string, null: false
+    end
+
+    create unique_index(:base_image_tags, [:repo, :tag])
+  end
+end
