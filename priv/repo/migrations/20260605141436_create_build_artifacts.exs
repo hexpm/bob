e30e69426ac97cfaaa333c0bbd973389fc635ec4
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-06-05 16:15:02 +0200

    Add build_artifacts table

---
+++ priv/repo/migrations/20260605141436_create_build_artifacts.exs
@@ -0,0 +1,17 @@
+defmodule Bob.Repo.Migrations.CreateBuildArtifacts do
+  use Ecto.Migration
+
+  def change do
+    create table(:build_artifacts) do
+      add :kind, :string, null: false
+      add :arch, :string, null: false
+      add :os, :string, null: false
+      add :name, :string, null: false
+      add :ref, :string, null: false
+      add :sha256, :string, null: false
+      add :built_at, :utc_datetime_usec, null: false
+    end
+
+    create unique_index(:build_artifacts, [:kind, :arch, :os, :name])
+  end
+end
