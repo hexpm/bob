c5ee7947e346a89dae9d3648aa396763a71effd6
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-08-15 17:56:32 +0200

    Use Tidewave 0.8.4 from Hex

--- mix.exs
+++ mix.exs
@@ -27,10 +27,7 @@ defmodule Bob.Mixfile do
 
   defp deps() do
     [
-      {:tidewave,
-       github: "tidewave-ai/tidewave_phoenix",
-       ref: "0a5b104313ea090c71b2ca4dba3a461c27a684bb",
-       only: [:dev]},
+      {:tidewave, "~> 0.8.4", only: [:dev]},
       {:ecto_sql, "~> 3.12"},
       {:ex_aws_s3, "~> 2.0"},
       {:finch, "~> 0.19"},
